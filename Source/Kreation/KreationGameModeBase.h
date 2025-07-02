ec1901d678cc9c619070fa0d989ea8563803e33e
Richard Osborne <rosborne255@yahoo.com>
2025-07-02 07:00:55 -0500

    add base game mode

---
+++ Source/Kreation/KreationGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KreationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KREATION_API AKreationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AKreationGameModeBase();
+};
